21ac0ac1670e121dfc796034de2737766bcc6f60
SunnatilloKamolov <sunnatillokamolov600@gmail.com>
2023-04-16 09:59:05 +0500

    CommitPrinter

--- src/main/java/uz/pdp/apppcmarket1/entity/Printer.java
+++ src/main/java/uz/pdp/apppcmarket1/entity/Printer.java
@@ -9,7 +9,7 @@ import uz.pdp.apppcmarket1.entity.template.AbsEntity;
 @Data
 @Entity
 public class Printer extends AbsEntity {
-    private String brand;
+    private String brand;//O'zgartirish
 
     private String printColor;
 
